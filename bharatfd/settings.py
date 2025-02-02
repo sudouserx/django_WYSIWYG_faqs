5acfb7d3dbb64ee5030d38775898915d1e7d04ab
sudouserx <sudouserx@gmail.com>
2025-02-02 11:20:05 +0530

    chore: Add 0.0.0.0 to ALLOWED_HOSTS

--- bharatfd/settings.py
+++ bharatfd/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7bk124$h#(f^u*d38=j!9byl24j#o&63q@bs*h6g8ezx^8c@3f"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
